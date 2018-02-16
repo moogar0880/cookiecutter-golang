953711e83e8228ae4c329247e738e14d0ab53976
moogar0880 <moogar0880@gmail.com>
2018-02-16 00:21:13 -0500

    fix versioning and Gopkg.toml

--- {{cookiecutter.app_name}}/version.go
+++ {{cookiecutter.app_name}}/version.go
@@ -2,12 +2,12 @@ package main
 
 // GitCommit is the git commit from which this that binary was compiled.
 // This will be filled in by the compiler.
-const GitCommit = ""
+var GitCommit string
 
 // Version is the tag from which this binary was compiled.
-const Version = ""
+var Version string
 
 // VersionPrerelease is a pre-release marker for the version of this binary.
 // If this is "" (empty string) then it means that it is a final release.
 // Otherwise, this is a pre-release such as "dev" (in development)
-const VersionPrerelease = ""
+var VersionPrerelease string
