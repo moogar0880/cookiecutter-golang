f2fd1adbd9ae753557d5d6dca6be6e589ef4179d
moogar0880 <moogar0880@gmail.com>
2018-02-21 00:10:55 -0500

    add ability to denote between library and service

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -42,39 +42,54 @@ def remove_docker_files():
     """
     Removes files needed for docker if it isn't going to be used
     """
-    for filename in ["Dockerfile",]:
-        os.remove(os.path.join(
+    for filename in ["Dockerfile"]:
+        path = os.path.join(
             PROJECT_DIRECTORY, filename
-        ))
+        )
+        if os.path.exists:
+            os.remove(path)
 
 
 def remove_viper_files():
     """
     Removes files needed for viper config utils
     """
-    shutil.rmtree(os.path.join(
+    path = os.path.join(
         PROJECT_DIRECTORY, "config"
-    ))
+    )
+
+    if os.path.exists(path):
+        shutil.rmtree(path)
 
 
 def remove_logrus_files():
     """
     Removes files needed for viper config utils
     """
-    shutil.rmtree(os.path.join(
+    path = os.path.join(
         PROJECT_DIRECTORY, "log"
-    ))
+    )
 
+    if os.path.exists(path):
+        shutil.rmtree(path)
+
+# remove service specific files if we're writing a library
+if '{{ cookiecutter.project_type }}'.lower() == 'library':
+    remove_viper_files()
+    remove_logrus_files()
+    remove_docker_files()
+    remove_file('main.go')
+    remove_file('version.go')
 
 # 1. Remove Dockerfiles if docker is not going to be used
 if '{{ cookiecutter.use_docker }}'.lower() != 'y':
     remove_docker_files()
 
-# 2. Remove viper config if not seleted
+# 2. Remove viper config if not selected
 if '{{ cookiecutter.use_viper_config }}'.lower() != 'y':
     remove_viper_files()
 
-# 3. Remove logrus utils if not seleted
+# 3. Remove logrus utils if not selected
 if '{{ cookiecutter.use_logrus_logging }}'.lower() != 'y':
     remove_logrus_files()
 
@@ -93,6 +108,7 @@ if '{{ cookiecutter.use_git }}'.lower() == 'y':
 else:
     remove_file(".gitignore")
 
+# Remove unused vendor related tools
 if '{{ cookiecutter.vendor_tool }}'.lower() == 'glide':
     remove_file('Gopkg.toml')
 elif '{{ cookiecutter.vendor_tool }}'.lower() == 'dep':
